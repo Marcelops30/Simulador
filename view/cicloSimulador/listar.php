7fbb9cb6634e9a0ad8d22ac1ad5e39e036a7040a
Marcelops30 <marceloklp27@gmail.com>
2017-09-01 10:09:46 -0300

    Update listar.php

--- view/cicloSimulador/listar.php
+++ view/cicloSimulador/listar.php
@@ -31,8 +31,8 @@
             </td>
             
             <td>
-                 <a href="<?= base_url('cicloSimulador/editar/'.$dados->id_csm) ?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>Editar</a>
-                <a href="<?= base_url('cicloSimulador/deletar/'.$dados->id_csm) ?>"><i class="fa fa-trash-o" aria-hidden="true"></i>Deletar</a>
+                 <a class="btn btn-warning" href="<?= base_url('cicloSimulador/editar/'.$dados->id_csm) ?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>Editar</a>
+                <a class="btn btn-danger" href="<?= base_url('cicloSimulador/deletar/'.$dados->id_csm) ?>"><i class="fa fa-trash-o" aria-hidden="true"></i>Deletar</a>
             </td>
         </tr>
         <?php endforeach; ?>
